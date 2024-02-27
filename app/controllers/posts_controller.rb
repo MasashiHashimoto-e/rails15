6d24cd0c02930630ef924279802a6b511a706f0d
MasashiHashimoto-c <miwasca0531@icloud.com>
2024-02-27 14:08:29 +0900

    編集、削除リンク、部屋の合計作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @schedule_total = Post.all.count
   end
 
   def new
@@ -8,7 +9,7 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.new(params.require(:post).permit(:user_id, :title, :startday, :finishyday, :oneday, :memo)) 
+    @post = Post.new(params.require(:post).permit(:user_id, :title, :startday, :finishday, :oneday, :memo)) 
     if @post.save
       flash[:notice] = "新規登録に成功しました"
       redirect_to :posts
@@ -16,6 +17,32 @@ class PostsController < ApplicationController
       flash[:notice] = "新規登録に失敗しました"
       render "new"
     end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:user_id, :title, :startday, :finishday, :oneday, :memo)) 
+      flash[:notice] = "スケジュールを更新しました"
+      redirect_to :posts
+    else
+      flash[:notice] = "スケジュールの更新に失敗しました"
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+     @post.destroy
+     flash[:notice] = "ユーザーを削除しました"
+     redirect_to :posts
   end
 end
  
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: { maximum: 20}
+    validates :startday, presence: true 
+    validates :finishday, presence: true
+    validates :memo, length: { maximum: 500 }
+    validate :start_finish_check
+    def start_finish_check
+        return false if finishday.blank? || startday.blank?
+        if startday > finishday
+            errors.add(:finishday, "は開始日より後の日付を選択してください")
+        end
+    end
 end
